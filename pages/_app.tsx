37138366afc7ba114dc36957c3a4eb5b2408b509
hermanwongkm <herman_wong_km@outlook.com>
2021-09-26 02:02:15 +0800

    Fix(CSS): Add chakara and set to use REM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const color = {
+  brand: {
+    900: "#1a365d"
+  }
 }
-export default MyApp
+
+const theme = extendTheme({ color })
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider >
+  )
+}
+export default App
