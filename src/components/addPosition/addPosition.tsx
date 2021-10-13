b586661480a116c74af428f182458c1b3db53f79
hermanwongkm <herman_wong_km@outlook.com>
2021-10-14 00:43:00 +0800

    skeleton code for form

--- src/components/addPosition/addPosition.tsx
+++ src/components/addPosition/addPosition.tsx
@@ -1,35 +1,45 @@
 import { FormControl, FormLabel, Input, NumberInput, NumberInputField, NumberDecrementStepper, NumberIncrementStepper, NumberInputStepper, Button, } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { Controller, useForm } from 'react-hook-form';
-import ReactDatePicker from "react-datepicker";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
+import styles from './addTransaction.module.css'
 
 const AddPositionForm = () => {
     const { register, handleSubmit, control } = useForm();
-
     const onSubmit = (data) => {
         console.log(data);
 
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.wrapper}>
+            <form className={styles.formWrapper} onSubmit={handleSubmit(onSubmit)}>
                 <FormControl>
-                    <FormLabel>Stock Name</FormLabel>
-                    <Input type='text' placeholder='AAPL' {...register("symbol")} />
-
-                    <Controller
-                        control={control}
-                        name="ReactDatepicker"
-                        render={({ field: { onChange, onBlur, value, ref } }) => (
-                            <ReactDatePicker
-                                onChange={onChange}
-                                onBlur={onBlur}
-                                selected={value}
+                    <div className={styles.rowGroup}>
+                        <div className={styles.formItem}>
+                            <FormLabel>Stock Name</FormLabel>
+                            <Input type='text' placeholder='AAPL' {...register("symbol")} />
+                        </div>
+                        <div className={styles.formItem}>
+                            <FormLabel>Date</FormLabel>
+                            <Controller
+                                control={control}
+                                name="ReactDatepicker"
+                                render={({ field: { onChange, onBlur, value, ref } }) => (
+                                    <DatePicker
+                                        wrapperClassName={styles.datePicker}
+                                        dateFormat="dd/MM/yyyy"
+                                        todayButton="Today"
+                                        onChange={onChange}
+                                        onBlur={onBlur}
+                                        selected={value}
                             />
-                        )}
-                    />
+                                )}
+                            />
+                        </div>
+                    </div>
                     <FormLabel>Quantity</FormLabel>
                     <NumberInput min={1}>
                         <NumberInputField placeholder='100' {...register("quantity")} />
