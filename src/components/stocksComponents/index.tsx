5d443bb9a2a2de7ff89bd15cc4ae40458bca27fc
hermanwongkm <herman_wong_km@outlook.com>
2022-03-05 00:48:44 +0800

    [Chore] Cleanup unused code

--- src/components/stocksComponents/index.tsx
+++ src/components/stocksComponents/index.tsx
@@ -1,23 +1,12 @@
-import { useQuery } from "urql";
 import React from "react";
 
 import AddTransaction from "./addTransaction/addTransaction";
-import getStockTransactions from "../../graphql/queries/stocks/getStocksTransactions";
 import StocksTable from "./stockTable/stockTable";
 
 const StocksPage = () => {
-  const [result, reexecuteQuery] = useQuery({
-    query: getStockTransactions,
-    pause: false,
-  });
-
-  const { data, fetching, error } = result;
-  if (fetching) return <p>Loading...</p>;
-  if (error) return <p>Oh no... {error.message}</p>;
-
   return (
     <div>
-      <AddTransaction execute={reexecuteQuery} />
+      <AddTransaction />
       <StocksTable />
     </div>
   );
